c5e2630d73c85fc71d0647f8206df7ddc384eccc
Vyacheslav Galymov <vgalymov@cern.ch>
2022-11-18 12:12:22 +0100

    added selector for TPCs

--- WireDumpDraw.py
+++ WireDumpDraw.py
@@ -45,8 +45,10 @@ class geo_tpc:
         
             
 
-def init( fname ):
-    tpcs = [geo_tpc(0), geo_tpc(1), geo_tpc(2), geo_tpc(3)]
+def init( fname, ntpc):
+    #tpcs = [geo_tpc(0), geo_tpc(1), geo_tpc(2), geo_tpc(3)]
+    tpcs = [ geo_tpc(i) for i in range( ntpc ) ]
+
     for t in tpcs:
         t.add_plane( t._id, 0)
         t.add_plane( t._id, 1)
@@ -76,10 +78,10 @@ def init( fname ):
             
 
 
-    for t in tpcs:
-        print(t.n())
-        for p in t._planes:
-            print('  ',p.n())
+    #for t in tpcs:
+    #    print(t.n())
+    #    for p in t._planes:
+    #        print('  ',p.n())
 
     return tpcs
 
@@ -93,40 +95,48 @@ if __name__=='__main__':
 
     parser.add_argument('-s', '--skip', help='skip wires',
                         default = 16, type=int)
+    parser.add_argument('-n', '--ntpc', help='number of tpc volumes',
+                        default = 4, type=int)
+    parser.add_argument('-t', '--tpc_ids', nargs='+', type=int,
+                        default = [] )
     args  = parser.parse_args()
 
+    tpcids = args.tpc_ids
+    if( not tpcids ): tpcids = [ i for i in range(args.ntpc) ]
     
-    tpcs = init( args.file )
-    fig, ax2d = plt.subplots( nrows = 1, ncols = 1, figsize=(16,10),
-                              sharex = 'col', sharey = 'row' )
-    axs = [ax2d, ax2d, ax2d, ax2d]
-    axs[0].set(xlabel="y (cm)", ylabel="z (cm")
-    axs[2].set(ylabel="z (cm)")
-    axs[1].set(xlabel="y (cm)")
+    tpcs = init( args.file, args.ntpc )
+    fig, ax = plt.subplots( figsize=(16,10) )
+    ax.set(xlabel="y (cm)", ylabel="z (cm")
     
     plot_plane = args.plane
     plot_wskip = args.skip  # draw every nth ch
-    fig.suptitle(f"{args.file}: Plot plane {plot_plane}")
-        
+    fig.suptitle(f"{args.file}: Plot plane {plot_plane} TPCs {tpcids}")
+    
     clrs = ['r', 'b', 'g', 'm']
+    cntr = 0
     for t in tpcs:
-        #if( t._id in (2, 3) ): continue
+        if( not t._id in tpcids ): continue
         p  = t._planes[plot_plane]
         ws = p._wires
+        color = clrs[cntr % 4]
+        cntr += 1
+
         for i in range(len(ws)):
-            if( i % plot_wskip != 0 and i != (len(ws)+1) ): continue
+            #if( i % plot_wskip != 0 and i != (len(ws)-1) ): continue
+            if( ws[i]._ch % plot_wskip != 0 ): continue
+            
             xpos = [ ws[i]._endpts[0][0], ws[i]._endpts[1][0] ]
             ypos = [ ws[i]._endpts[0][1], ws[i]._endpts[1][1] ]
-            axs[t._id].plot(xpos, ypos, color= clrs[t._id])
+            ax.plot(xpos, ypos, color=color)
             if( plot_plane == 0 ):
-                axs[t._id].annotate( f"{ws[i]._ch}:{ws[i]._id}",
-                                     [0.5*(xpos[0]+xpos[1]), 0.5*(ypos[0]+ypos[1])],  fontsize=7)
+                ax.annotate( f"{ws[i]._ch}:{ws[i]._id}",
+                             [0.5*(xpos[0]+xpos[1]), 0.5*(ypos[0]+ypos[1])],  fontsize=7)
             elif( plot_plane == 1):
-                axs[t._id].annotate( f"{ws[i]._ch}:{ws[i]._id}",
-                                     [0.5*(xpos[0]+0.99*xpos[1]), 0.5*(ypos[0]+ypos[1])],  fontsize=7, rotation = 90)
+                ax.annotate( f"{ws[i]._ch}:{ws[i]._id}",
+                             [0.5*(xpos[0]+0.99*xpos[1]), 0.5*(ypos[0]+ypos[1])],  fontsize=7) #, rotation = 90)
             else:
-                axs[t._id].annotate( f"{ws[i]._ch}:{ws[i]._id}",
-                                    [0.5*(xpos[0]+xpos[1]), 0.5*(ypos[0]+ypos[1])],  fontsize=7)
+                ax.annotate( f"{ws[i]._ch}:{ws[i]._id}",
+                             [0.5*(xpos[0]+xpos[1]), 0.5*(ypos[0]+ypos[1])],  fontsize=7) #, rotation = 90)
                                 
     #
     #
